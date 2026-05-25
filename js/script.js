b5fb5f3ff8e2bcbcea30a814ed9d75fb292922c5
Koti suresh <kommukotisuresh@gmail.com>
2026-05-25 10:31:44 +0530

    added smooth functionality

--- js/script.js
+++ js/script.js
@@ -77,3 +77,17 @@ function filterProjects(type) {
     });
 
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  });
+});
